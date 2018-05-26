ccfbf4623fe62af64daba3765716c4ad7c047895
Ian C. Anderson <ian@iancanderson.com>
2018-05-26 14:36:14 -0400

    tweaks

--- app/models/trophy.rb
+++ app/models/trophy.rb
@@ -1,7 +1,7 @@
 class Trophy < ApplicationRecord
   belongs_to :user
 
-  validates :bjcp_score, inclusion: { in: 1..50 }
+  validates :bjcp_score, numericality: { greater_than: 0, less_than: 51 }
   validates :competition_date, presence: true
   validates :competition_url, presence: true
   validates :place, inclusion: { allow_nil: true, in: 1..4 }
