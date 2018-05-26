ff6d2670b6340a056840899015275c2ec111fcba
Ian C. Anderson <ian@iancanderson.com>
2018-05-26 15:19:59 -0400

    Configure sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,17 @@ Rails.application.configure do
   config.log_level = :debug
   config.log_tags = [ :request_id ]
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => ENV.fetch('APPLICATION_HOST'),
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
