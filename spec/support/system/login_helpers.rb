04fb4557f7e8d98c24b91ddf81822580ecc28b50
Ian C. Anderson <ian@iancanderson.com>
2018-05-27 08:28:15 -0400

    Extract login method for system specs

---
+++ spec/support/system/login_helpers.rb
@@ -0,0 +1,24 @@
+module System
+  def login_as(user)
+    visit "/"
+
+    click_link "Add Trophy"
+
+    # Submit login form with email address in worts list
+    expect(current_path).to eq("/users/sign_in")
+
+    fill_in "Worts email", with: user.email
+    click_on "Send magic link"
+
+    # Open email, visit link in email to log in
+    emails = ActionMailer::Base.deliveries
+    expect(emails.size).to eq(1)
+
+    magic_link_url = emails.first.body.raw_source.match(/http:\/\/[\S]+/)
+
+    visit magic_link_url
+
+    # It redirects to root in test because the session is cleared out
+    # In development, it redirects you back to where you wanted to go initially
+  end
+end
--- spec/system/submit_trophy_spec.rb
+++ spec/system/submit_trophy_spec.rb
@@ -3,33 +3,14 @@ require "rails_helper"
 describe "submit trophy" do
   it "succeeds with required fields after logging in via email" do
     worts_member = create(:user)
+    login_as(worts_member)
 
-    visit "/"
-
-    click_link "Add Trophy"
-
-    # Submit login form with email address in worts list
-    expect(current_path).to eq("/users/sign_in")
-
-    fill_in "Worts email", with: worts_member.email
-    click_on "Send magic link"
-
-    # Open email, visit link in email to log in
-    emails = ActionMailer::Base.deliveries
-    expect(emails.size).to eq(1)
-
-    magic_link_url = emails.first.body.raw_source.match(/http:\/\/[\S]+/)
-
-    visit magic_link_url
-
-    # It redirects to root in test because the session is cleared out
-    # In development, it redirects you back to where you wanted to go initially
     click_link "Add Trophy"
 
     # Now we're logged in so it doesn't redirect
     expect(current_path).to eq("/trophies/new")
 
-    fill_in :trophy_bjcp_score, with: 25
+    select 25, from: :trophy_bjcp_score
 
     competition_date = Date.yesterday
     select competition_date.year, from: :trophy_competition_date_1i
@@ -46,7 +27,7 @@ describe "submit trophy" do
     expect(page).to have_content(worts_member.full_name)
   end
 
-  it "doesn't send magic link to non-worts member" do
+  it "doesn't email a magic link to non-worts member" do
     visit "/"
 
     click_link "Add Trophy"
