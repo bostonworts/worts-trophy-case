41a785a9acdc1e537e2da24d2b94ca7ca686f8f0
Ian C. Anderson <ian@iancanderson.com>
2018-05-27 16:37:42 -0400

    Require full_name to be present

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :full_name, presence: true, uniqueness: true
 
   passwordless_with :email
 
