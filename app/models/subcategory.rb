342037e4f6d7a3b1a1c7937eda332edbb12c7455
Ian C. Anderson <ian@iancanderson.com>
2018-05-28 22:08:54 -0400

    Sort subcategories by number, then alphabetically

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -5,6 +5,16 @@ class Subcategory < ApplicationRecord
   validates :category, presence: true
   validates :name, presence: true
 
+  def self.sorted
+    all.sort_by do |sub|
+      if sub.bjcp_id.to_i > 0
+        [sub.bjcp_id.to_i, sub.bjcp_id]
+      else
+        [99, sub.bjcp_id]
+      end
+    end
+  end
+
   def description
     "#{bjcp_id} - #{name}"
   end
--- spec/models/subcategory_spec.rb
+++ spec/models/subcategory_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Subcategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".sorted" do
+    it "sorts 10A after 2A" do
+      bjcp_ids = Subcategory.sorted.map(&:bjcp_id)
+
+      expect(bjcp_ids.index("10A")).to be > bjcp_ids.index("2A")
+    end
+
+    it "sorts M3B after 10A" do
+      bjcp_ids = Subcategory.sorted.map(&:bjcp_id)
+
+      expect(bjcp_ids.index("M3B")).to be > bjcp_ids.index("10A")
+    end
+  end
 end
